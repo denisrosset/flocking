e45e9cd195a034084c28722857d008e0799ade1f
denis <denis@ed29ea54-27db-417a-a463-1693368b5776>
2009-01-28 04:29:29 +0000

    correct bug
    git-svn-id: svn+ssh://denisrosset.dyndns.org/home/svn/2008_master/2009@413 ed29ea54-27db-417a-a463-1693368b5776

--- sims/vicsek1995ntp/process.py
+++ sims/vicsek1995ntp/process.py
@@ -3,4 +3,4 @@ sys.path.append('../..')
 import flocking.sim
 folder = flocking.sim.LocalFolder('/Users/denisrosset/Projects/Uni/Master/2009/sims/vicsek1995ntp/data')
 batch = flocking.sim.Batch.load_from_folder(folder)
-flocking.sim.ParallelProcessing().process(batch, address = ('75.101.161.126', 50004))
+flocking.sim.ParallelProcessing(address = ('75.101.161.126', 50004)).process(batch)
